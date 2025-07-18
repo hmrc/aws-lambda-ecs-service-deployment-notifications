b6b8d8a07394a69a193a37d42494f53a30e60f01
Sam Pickering <83871143+samhpickering@users.noreply.github.com>
2025-07-18 13:55:15 +0100

    INFRA-7949: Add test for multiple notifications
    The Lambda is designed to produce multiple notifications if multiple
    service ARNs are passed in the deployment event, however we were not
    testing for this.
    
    Add a test to check that multiple service ARNs produce multiple
    notifications.

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -132,3 +132,64 @@ def test_handler_does_not_send_notification_for_other_clusters(
         handler(event, context)
 
     mock_send_notification.assert_not_called()
+
+
+@unittest.mock.patch("ecs_service_deployment_notifications.slack.send_notification")
+@unittest.mock.patch("ecs_service_deployment_notifications.ecs.ServiceArn")
+def test_handler_sends_notification_for_each_resource(
+    mock_service_arn: unittest.mock.MagicMock,
+    mock_send_notification: unittest.mock.MagicMock,
+):
+    cluster_name = "notified-cluster-name"
+    service_name = "service-name"
+    slack_channel = "test-channel"
+    lambda_arn = "arn:aws:lambda:eu-west-2:123456789012:function:example-function:1"
+
+    event = {
+        "resources": [
+            f"arn:MOCKED:service/{cluster_name}/{service_name}-1",
+            f"arn:MOCKED:service/{cluster_name}/{service_name}-2",
+        ],
+        "detail": {
+            "eventName": "SERVICE_DEPLOYMENT_IN_PROGRESS",
+            "reason": "No reason",
+        },
+    }
+    context = {}
+
+    mock_service_arn.side_effect = [
+        MockServiceArn(cluster_name=cluster_name, service_name=f"{service_name}-1"),
+        MockServiceArn(cluster_name=cluster_name, service_name=f"{service_name}-2"),
+    ]
+
+    with unittest.mock.patch.dict(
+        os.environ,
+        {
+            "CLUSTER_NAME": cluster_name,
+            "SLACK_CHANNEL": slack_channel,
+            "SLACK_NOTIFICATIONS_LAMBDA_ARN": lambda_arn,
+        },
+        clear=True,
+    ):
+        handler(event, context)
+
+    assert mock_send_notification.call_args_list == [
+        unittest.mock.call(
+            lambda_arn=lambda_arn,
+            description="ECS service deployment in progress",
+            color=None,
+            channel=slack_channel,
+            cluster_name=cluster_name,
+            service_name=f"{service_name}-1",
+            reason="No reason",
+        ),
+        unittest.mock.call(
+            lambda_arn=lambda_arn,
+            description="ECS service deployment in progress",
+            color=None,
+            channel=slack_channel,
+            cluster_name=cluster_name,
+            service_name=f"{service_name}-2",
+            reason="No reason",
+        ),
+    ]
